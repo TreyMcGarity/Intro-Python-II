81ba1cb2364914aa7c484069fd41b3a107971ab5
Trey McGarity <trey.mcgarity99@gmail.com>
2020-08-07 16:12:25 -0500

    1st day mvp

--- src/adv.py
+++ src/adv.py
@@ -1,5 +1,5 @@
 from room import Room
-
+from player import Player
 # Declare all the rooms
 
 room = {
@@ -38,14 +38,34 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
-
+player = Player(room["outside"])
 # Write a loop that:
 #
 # * Prints the current room name
 # * Prints the current description (the textwrap module might be useful here).
 # * Waits for user input and decides what to do.
 #
+while True:
+    print(f"{player.current_room.name},\n{player.current_room.description}")
+    travel_to = input('(Press "n" fo North, "e" for East, "s" for South, "w" for West, "q" to quit)\nWhere to?')
+    print("______________________________________________________________")
 # If the user enters a cardinal direction, attempt to move to the room there.
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+    if travel_to == 'n' or travel_to == 'e' or travel_to == 's' or travel_to == 'w' or travel_to == 'q':
+        if travel_to == "n" and player.current_room.n_to:
+            player.current_room = player.current_room.n_to
+        elif travel_to == "e" and player.current_room.e_to:
+            player.current_room = player.current_room.e_to
+        elif travel_to == "s" and player.current_room.s_to:
+            player.current_room = player.current_room.s_to
+        elif travel_to == "w" and player.current_room.w_to:
+            player.current_room = player.current_room.w_to
+        elif travel_to == "q":
+            print("You Left the Game")
+            break
+        else:
+            print("not an option")
+    else:
+        print("Enter one of the optional directions.\n")
